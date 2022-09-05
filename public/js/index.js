99a68abf66899025297e05d4b018830beeec6fa0
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-05 03:28:19 +0300

    handle request get allpost in home page and build dom relates #10

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,53 @@ const signupConfirmPassword = document.querySelector('#sign-up .confirm-password
 
 const signupButton = document.querySelector('#sign-up button');
 
+const allPosts = document.querySelector('#all-posts');
+
+fetch('/allPosts')
+  .then((data) => data.json())
+  .then((res) => {
+    console.log(res);
+    res.forEach((ele) => {
+      const postCard = document.createElement('div');
+      postCard.setAttribute('class', 'post-card');
+      allPosts.appendChild(postCard);
+
+      const profileUsernameContainer = document.createElement('div');
+      profileUsernameContainer.setAttribute('class', 'profile-username-container');
+      postCard.appendChild(profileUsernameContainer);
+
+      const profileImage = document.createElement('img');
+      profileImage.setAttribute('class', 'profile-img');
+      profileImage.src = ele.profile_image;
+      profileUsernameContainer.appendChild(profileImage);
+
+      const username = document.createElement('p');
+      username.setAttribute('class', 'username');
+      username.textContent = ele.username;
+      profileUsernameContainer.appendChild(username);
+
+      const dateTime = document.createElement('p');
+      dateTime.setAttribute('class', 'date-time');
+      dateTime.textContent = ele.data_time;
+      postCard.appendChild(dateTime);
+
+      const postTitle = document.createElement('h2');
+      postTitle.setAttribute('class', 'post-title');
+      postTitle.textContent = ele.title;
+      postCard.appendChild(postTitle);
+
+      const postContent = document.createElement('p');
+      postContent.setAttribute('class', 'post-content');
+      postContent.textContent = ele.content;
+      postCard.appendChild(postContent);
+
+      const postImage = document.createElement('img');
+      postImage.setAttribute('class', 'post-img');
+      postImage.src = ele.post_image;
+      postCard.appendChild(postImage);
+    });
+  });
+
 signupButton.addEventListener('click', () => {
   const header = {
     method: 'POST',
