2a9561a56eeb37a602261bc086d04c05fd9d35f2
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-08 21:31:39 +0300

    handle some request from user profile as checkAuth and get posts relates #25

---
+++ public/js/userProfile.js
@@ -0,0 +1,36 @@
+const navigationUsername = document.querySelector('.userBx .username');
+
+const navigationUserImage = document.querySelector('.imgBx img');
+
+fetch('/userinfo')
+  .then((userInfo) => userInfo.json())
+  .then((userInfoResult) => {
+    navigationUsername.textContent = userInfoResult.username;
+    navigationUserImage.src = userInfoResult.profile_image;
+  });
+
+// fetch any user posts by id in url  || fetch currentUser posts by id in token
+const id = window.location.href.split('=')[1];
+if (id) {
+  fetch(`/userProfile/${id}`)
+    .then((data) => data.json())
+    .then((res) => {
+      console.log('user profile by params', res);
+      domForPosts(res);
+    });
+} else {
+  fetch('/userProfile/')
+    .then((data) => data.json())
+    .then((res) => {
+      console.log('user profile by token', res);
+      domForPosts(res);
+    });
+}
+
+//! dropDown menu
+
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.addEventListener('click', () => {
+  navigation.classList.toggle('active');
+});
