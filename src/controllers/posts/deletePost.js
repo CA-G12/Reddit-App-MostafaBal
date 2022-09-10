79a7d988bd0c3ab9e1ad99c56acc151b1088e5c0
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-10 12:32:39 +0300

    handle add post cases ;

--- src/controllers/posts/deletePost.js
+++ src/controllers/posts/deletePost.js
@@ -3,7 +3,7 @@ const { deletePostQuery } = require('../../database/queries');
 const deletePost = (req, res) => {
   const { postId } = req.params;
   deletePostQuery(postId)
-    .then((postIdResult) => res.json({ msg: 'deleted successfully' }))
+    .then((postIdResult) => res.json({ msg: 'deleted successfully', isDeleted: true }))
     .catch((err) => console.log(err));
 };
 
